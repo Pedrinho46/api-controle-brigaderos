b228c3cabc77168e4b811952791b7d4bc43164e8
Pedrinho46 <pedrocabralsantos26@gmail.com>
2025-05-03 11:06:04 -0300

    limpando

--- src/ingredients/dto/create-ingredient.dto.ts
+++ src/ingredients/dto/create-ingredient.dto.ts
@@ -1,1 +1,20 @@
-export class CreateIngredientDto {}
+import { IsString, IsNumber, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateIngredientDto {
+  @IsString()
+  @IsNotEmpty()
+  nome: string;
+
+  @IsNumber()
+  preco_unitario: number;
+
+  @IsNumber()
+  quantidade_comprada: number;
+
+  @IsString()
+  @IsNotEmpty()
+  unidade: string;
+
+  @IsOptional()
+  data_compra?: Date;
+}
